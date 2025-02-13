59425cb258e6223e4afd6bc812f0cb61a56cc267
William Gay <williamgay360@gmail.com>
2025-02-12 19:28:38 -0500

    turning the command into a function

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import subprocess
 
-filenames = ['ajff.mp4', 'drum.mp4', 'hypernorm.mp4', 'bobbyken.mp4', 'markfisher.mp4', 'jbearly.mp4']
-urls = ['https://www.youtube.com/watch?v=AOCQp6lAfEE', 'https://www.youtube.com/watch?v=GiPe1OiKQuk', 'https://www.youtube.com/watch?v=AUiqaFIONPQ&t=80s', 'https://www.youtube.com/watch?v=DOiDUbaBL9E', 'https://www.youtube.com/watch?v=aCgkLICTskQ&t=602s', 'https://www.youtube.com/watch?v=rWRbTnE1PEM']
-
-for filename, url  in zip(filenames, urls):
-    print(filename)
+def download_video(filename: str, url: str) -> dict:
     command = ['udown', '-f', filename, url]
-
     try:
         result = subprocess.run(
             command,
@@ -15,14 +10,21 @@ for filename, url  in zip(filenames, urls):
             text=True,
             check=True
         )
-        print({
+        return {
             'success': True,
             'output': result.stdout,
             'error': result.stderr
-        })
+        }
     except subprocess.CalledProcessError as e:
-        print({
+        return {
             'success': False,
             'output': e.stdout,
             'error': e.stderr
-        })
+        }
+
+if __name__ == '__main__':
+    filenames = ['ajff.mp4', 'drum.mp4', 'hypernorm.mp4', 'bobbyken.mp4', 'markfisher.mp4', 'jbearly.mp4']
+    urls = ['https://www.youtube.com/watch?v=AOCQp6lAfEE', 'https://www.youtube.com/watch?v=GiPe1OiKQuk', 'https://www.youtube.com/watch?v=AUiqaFIONPQ&t=80s', 'https://www.youtube.com/watch?v=DOiDUbaBL9E', 'https://www.youtube.com/watch?v=aCgkLICTskQ&t=602s', 'https://www.youtube.com/watch?v=rWRbTnE1PEM']
+    
+    for filename, url  in zip(filenames, urls):
+        download_video(filename, url)
